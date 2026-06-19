953cc17b563e1ae0ec4f4c82454a96d5860f1442
Sabrina <161891067+Mangle707@users.noreply.github.com>
2026-06-19 17:05:20 -0300

    Add files via upload

---
+++ Projeto-pa/projeto-lanchonete/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+
+        <>
+        <h1>Lanchonete</h1>
+        <h2>O melhor da regiao</h2>
+        </>
+        
+    )
+}
+export default Header 
+//export sem precia estar no final para poder ser exportado
---
+++ Projeto-pa/projeto-lanchonete/src/components/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return(
+        <>
+        <h1>Login</h1>
+        <h2>Digite seu nome: </h2>
+        <h2>Digite sua idade: </h2>
+        </>
+    )
+}
+export default Login
