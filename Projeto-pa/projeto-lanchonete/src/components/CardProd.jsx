bede5ac46c3825bc48cfb96d5fe9d2fb42f96c82
sabrina <magicline42@gmail.com>
2026-07-29 16:44:02 -0300

    atualizaçao depois das ferias

--- Projeto-pa/projeto-lanchonete/src/components/CardProd.jsx
+++ Projeto-pa/projeto-lanchonete/src/components/CardProd.jsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
 import "./CardProd.css";
 
+//mudar as imagens aqui e arrumar para aparecerem
 function CardProd() {
   const [produtos, setProdutos] = useState([
     {
-      nome: "X-Salada",
-      descricao: "Pão, hambúrguer, queijo, alface, tomate e maionese.",
-      preco: 18,
-      quantidade: 0,
+    imagem: "https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.instagram.com%2Fp%2FDC6nWqauFGJ%2F&ved=0CBYQjRxqFwoTCICd0bvR-JUDFQAAAAAdAAAAABA3&opi=89978449",
+    nome: "X-Salada",
+    descricao: "Pão, hambúrguer, queijo, alface, tomate e maionese.",
+    preco: 18,
+    quantidade: 0
     },
     {
+      imagem: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-x-bacon.jpg",//mudar a imagem novamente aqui
       nome: "X-Bacon",
       descricao: "Hambúrguer, bacon crocante, queijo e molho especial.",
       preco: 22,
       quantidade: 0,
     },
     {
+      imagem: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-x-tudo.jpg",//mudar a imagem novamente aqui
       nome: "X-Tudo",
       descricao: "Hambúrguer duplo, bacon, ovo, queijo, presunto e salada.",
       preco: 28,
       quantidade: 0,
     },
     {
+      imagem: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-batata-frita.jpg",//mudar a imagem novamente aqui
       nome: "Batata Frita",
       descricao: "Porção de batatas crocantes e salgadas.",
       preco: 12,
       quantidade: 0,
     },
     {
+      imagem: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-hot-dog.jpg",//mudar a imagem novamente aqui
       nome: "Hot Dog",
       descricao: "Pão, salsicha, milho, batata palha e molho.",
       preco: 10,
@@ -61,6 +67,7 @@ function CardProd() {
       <div className="container">
         {produtos.map((produto, index) => (
           <div className="card" key={index}>
+            <img src={produto.imagem} alt={produto.nome} className="imagem" />
             <h2>{produto.nome}</h2>
 
             <p>{produto.descricao}</p>
